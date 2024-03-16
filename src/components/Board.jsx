a0588d82dd5e3af7a1d2d3a5d709f3e8031e485a
John O'Brien <johnobrien.dev@gmail.com>
2024-03-16 16:33:53 -0400

    add containers and state to list component

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -6,9 +6,24 @@ function Board(){
     const [cards, setCards] = useState([]);
     return(
         <div className="board">
-           <List/>
-           <List/>
-           <List/>
+           <List
+            title="Backlog"
+            list="backlog"
+            cards={cards}
+            setCards={setCards}
+           />
+          <List
+            title="In Progress"
+            list="inProgress"
+            cards={cards}
+            setCards={setCards}
+           />
+           <List
+            title="Complete"
+            list="Complete"
+            cards={cards}
+            setCards={setCards}
+           />
         </div>
     )
 } 
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
+import "./styles/List.css";
+
 function List(title, list, cards, setCards){
+    const [active, setActive] = useState(false);
     return(
-        <div>
+        <div className="list-container">
+            <div className="list-title-container">
+                <h3>{title}</h3>
+                <span>{cards.length}</span>
+            </div>
+            <div className="cards-container">
+
+            </div>
 
         </div>
     )
