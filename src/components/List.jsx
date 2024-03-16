67450e69ba80015008b8f951e2352fbb74ebe717
John O'Brien <johnobrien.dev@gmail.com>
2024-03-16 17:14:56 -0400

    fix error in list props, added {}

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,15 +1,20 @@
 import { useState } from "react";
 import "./styles/List.css";
+import Card from "./Card";
 
-function List(title, list, cards, setCards){
+function List({title, list, cards, setCards}){
     const [active, setActive] = useState(false);
+    const filteredCards = cards.filter((card) => card.list === list);
     return(
         <div className="list-container">
             <div className="list-title-container">
                 <h3>{title}</h3>
-                <span>{cards.length}</span>
+                <span>{filteredCards.length}</span>
             </div>
             <div className={`cards-container ${active ? "list-active" : "list-inactive"}`}>
+                {filteredCards.map((c) =>{
+                    <Card  key={c.id}{...c}/>;
+                })}
 
             </div>
 
