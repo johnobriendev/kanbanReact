e4acb237a6e2c32f2fed8d90c61f4a3d533b6ae0
John O'Brien <johnobrien.dev@gmail.com>
2024-03-17 19:13:30 -0400

    add getNearestIndicator

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -26,8 +26,36 @@ function List({title, list, cards, setCards}){
         lineIndicator(e);
         setActive(true);
     }
-    const lineIndicator =(e) =>{
+    const lineIndicator = (e) =>{
+        const indicators = getIndicators();
+        const el = getNearestIndicator(e, indicators);
+        el.element.style.opacity = "1";
+    }
+    const getNearestIndicator = (e, indicators) => {
+        const DISTANCE_OFFSET = 50;
 
+        const el = indicators.reduce(
+          (closest, child) => {
+            const box = child.getBoundingClientRect();
+    
+            const offset = e.clientY - (box.top + DISTANCE_OFFSET);
+    
+            if (offset < 0 && offset > closest.offset) {
+              return { offset: offset, element: child };
+            } else {
+              return closest;
+            }
+          },
+          {
+            offset: Number.NEGATIVE_INFINITY,
+            element: indicators[indicators.length - 1],
+          }
+        );
+    
+        return el;
+    }
+    const getIndicators = () =>{
+        return Array.from(document.querySelectorAll(`[data-list="${list}"]`));
     }
     const handleDragLeave = (e) => {
         setActive(false);
