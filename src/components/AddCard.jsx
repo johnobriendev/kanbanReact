695e7df4a005771256b8297fd9318d0d205b0e97
John O'Brien <johnobrien.dev@gmail.com>
2024-03-17 13:03:23 -0400

    add functionality to add card buttons

--- src/components/AddCard.jsx
+++ src/components/AddCard.jsx
@@ -1,14 +1,40 @@
 import { useState } from "react"
 import "./styles/AddCard.css"
 
-function AddCard() {
+function AddCard({list, setCards}) {
     const [text, setText] = useState('');
     const [adding, setAdding] = useState(false);
+
+    const handleAddCard = (e) => {
+        e.preventDefault();
+        if (!text.trim().length) return;
+
+        const newCard = {
+            list,
+            title : text.trim(),
+            id: Math.random().toString(),
+        }
+
+        setCards((pv) => [...pv, newCard]);
+        setAdding(false);
+    }
 
     return(
         <>
         {adding ? ( 
-            <></>
+            <form onSubmit={handleAddCard}>
+                <textarea
+                    onChange={(e) =>  setText(e.target.value)}
+                    autoFocus
+                    placeholder="Add a new card"
+                    className="add-card-textarea"
+                >
+                </textarea>
+                <div>
+                    <button onClick={(e) => setAdding(false)}  className="close-textarea">Close</button>
+                    <button type="submit" className="add-textarea">Add +</button>
+                </div>
+            </form>
         ) : (
             <button className="add-card-btn" onClick={() => setAdding(true)}>
                 <span>Add Card</span>
